0d6766efad92f62853d3ec4329da0e6eb6a2420c
Peng <pzg1994@gmail.com>
2020-07-27 20:58:05 +0200

    update server

--- demo/src/main/java/com/test/demo/FileUploadController.java
+++ demo/src/main/java/com/test/demo/FileUploadController.java
@@ -1,25 +1,38 @@
 package com.test.demo;
 
+import org.apache.commons.io.IOUtils;
+import org.dcm4che3.io.DicomInputStream;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import java.io.IOException;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.awt.image.BufferedImage;
+import java.util.Iterator;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 
-//import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
-//import org.dcm4che3.data.Tag;
-//import org.dcm4che3.data.Attributes;
+import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.Attributes;
 
-
+@CrossOrigin(origins = "localhost:4200", maxAge = 3600)
 @RestController
 public class FileUploadController {
 
     //Save the uploaded file to this folder
     private static final String UPLOADED_FOLDER = "./";
-
+    private String PatientName;
     @GetMapping("/")
     public String index() {
         return "upload";
@@ -40,7 +53,32 @@ public class FileUploadController {
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
-
+            File myDicomFile = new File(path.toString());
+            System.out.println(path.toString());
+            DicomInputStream dis = new DicomInputStream(myDicomFile);
+            Attributes attr = dis.readDataset(-1, -1);
+            String patientName = attr.getString(Tag.PatientName, "");
+            System.out.println(patientName);
+            this.PatientName = patientName;
+            BufferedImage myJpegImage = null;
+            Iterator<ImageReader> iter = ImageIO.getImageReadersByFormatName("DICOM");
+            ImageReader reader = (ImageReader) iter.next();
+            DicomImageReadParam param = (DicomImageReadParam) reader.getDefaultReadParam();
+            try {
+                ImageInputStream iis = ImageIO.createImageInputStream(myDicomFile);
+                reader.setInput(iis, false);
+                myJpegImage = reader.read(0, param);
+                iis.close();
+                if (myJpegImage == null) {
+                    System.out.println("\nError: couldn't read dicom image!");
+                }
+                File myJpegFile = new File("./jpegImage.jpg");
+                OutputStream output = new BufferedOutputStream(new FileOutputStream(myJpegFile));
+                ImageIO.write(myJpegImage, "jpeg", output);
+                output.close();
+            }catch(IOException e) {
+                System.out.println("\nError: couldn't read dicom image!"+ e.getMessage());
+            }
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
@@ -48,17 +86,39 @@ public class FileUploadController {
             e.printStackTrace();
         }
 
-        return "redirect:/uploadStatus";
+        return this.PatientName;
     }
+
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/uploadStatus")
-    public String uploadStatus() {
-        return "uploadStatus";
+    @GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> image() throws IOException {
+        final ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(Paths.get(
+                "./jpegImage.jpg"
+        )));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(inputStream.contentLength())
+                .body(inputStream);
+
     }
-    @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+
+//    @CrossOrigin(origins = "http://localhost:4200")
+//    @GetMapping("/uploadStatus")
+//    public String uploadStatus() {
+//        return "uploadStatus";
+//    }
+//
+//
+//
+//    @GetMapping(
+//            value = "/image",
+//            produces = MediaType.IMAGE_JPEG_VALUE
+//    )
+//    @CrossOrigin(origins = "http://localhost:4200")
+//    public @ResponseBody byte[] getImageWithMediaType() throws IOException {
+//        InputStream in = getClass()
+//                .getResourceAsStream("./jpegImage.jpg");
+//        return IOUtils.toByteArray(in);
+//    }
 
 }
