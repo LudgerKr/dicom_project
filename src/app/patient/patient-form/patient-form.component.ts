b1905b71544945e5861f6e04deaa77cf54a6cfc5
dev09 <r.la@macway.com>
2020-07-27 12:27:28 +0200

    add upload files button

--- src/app/patient/patient-form/patient-form.component.ts
+++ src/app/patient/patient-form/patient-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { Router} from '@angular/router';
+import {HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-patient-form',
@@ -9,12 +10,36 @@ import { Router} from '@angular/router';
 })
 export class PatientFormComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  fileToUpload: Array <File>;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   patientForm(form: NgForm) {
+      console.log(form.value);
   }
 
+  handleFileInput(element) {
+    this.fileToUpload = element.target.files;
+  }
+
+  upload() {
+    const fileElement = document.getElementById('DICOMfile');
+    let fileExtension = '';
+    console.log(fileElement)
+    if (fileElement.value.lastIndexOf('.') > 0) {
+      fileExtension = fileElement.value.substring(fileElement.value.lastIndexOf('.') + 1, fileElement.value.length);
+    }
+    if (fileExtension.toLowerCase() === 'dcm') {
+      this.http.post('http://127.0.0.1:4200/upload', this.fileToUpload)
+          .subscribe((response) => {
+            console.log('response received is ', response);
+          });
+    } else {
+      alert('You must select a DICOM (.dcm) file for upload');
+      return false;
+    }
+  }
 }
