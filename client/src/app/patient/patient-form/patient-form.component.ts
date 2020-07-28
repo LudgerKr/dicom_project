2280ca0b90eb636ea1aa8ca69fb4863e25406daa
dev09 <r.la@macway.com>
2020-07-28 14:24:35 +0200

    replace pat into server and client folder, create dockerfile and docker-compos for test

---
+++ client/src/app/patient/patient-form/patient-form.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { Router} from '@angular/router';
+import {HttpClient  , HttpRequest,  HttpEvent} from '@angular/common/http';
+
+@Component({
+  selector: 'app-patient-form',
+  templateUrl: './patient-form.component.html',
+  styleUrls: ['./patient-form.scss']
+})
+export class PatientFormComponent implements OnInit {
+  img = null;
+  fileToUpload: Array <File>;
+  imageToShow: any;
+  // tslint:disable-next-line:ban-types
+  name: String;
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  handleFileInput(element) {
+    this.fileToUpload = element.target.files;
+  }
+
+  upload() {
+    const fileElement = document.getElementById('DICOMfile') as HTMLInputElement;
+    let fileExtension = '';
+    console.log(fileElement);
+    if (fileElement.value.lastIndexOf('.') > 0) {
+      fileExtension = fileElement.value.substring(fileElement.value.lastIndexOf('.') + 1, fileElement.value.length);
+    }
+    if (fileExtension.toLowerCase() === 'dcm') {
+      const formData: FormData = new FormData();
+
+      formData.append('file', this.fileToUpload[0]);
+      this.http.post('http://localhost:8080/upload', formData, { responseType: 'text' })
+          .subscribe((response) => {
+            this.name = response;
+          });
+    } else {
+      alert('You must select a DICOM (.dcm) file for upload');
+      return false;
+    }
+  }
+  getImage() {
+  this.http.get('http://localhost:8080/image', { responseType: 'blob' })
+    .subscribe(result => {
+      this.img = result;
+      console.log(result);
+      this.createImageFromBlob(this.img);
+    });
+
+  }
+
+  createImageFromBlob(image: Blob) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+       this.imageToShow = reader.result;
+       console.log(this.imageToShow);
+    }, false);
+
+    if (image) {
+       reader.readAsDataURL(image);
+    }
+ }
+
+}
